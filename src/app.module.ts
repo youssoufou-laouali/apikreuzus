18e7aa62addcc249c04168bdc59f1ae0f9b965f5
youssoufk <youssoufou-laouali@kreezus.com>
2021-06-11 09:45:55 +0000

    init prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { EventModule } from './event/event.module';
 
+
 @Module({
   imports: [EventModule],
   controllers: [AppController],
